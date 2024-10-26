8cd86439e0f0359dba47547ae1ca7fa4f41f493e
Michael <onisoris@yandex.ru>
2024-10-27 00:02:31 +0300

    перенос компиляции в setup.py в конец файла

--- setup.py
+++ setup.py
@@ -6,15 +6,6 @@ def readme():
     with open('README.md', 'r', encoding='utf-8') as f:
         return f.read()
 
-# Cython-модуль, который нужно скомпилировать
-extensions = [
-    Extension(
-        "pointsim.cython_pid",
-        ["pointsim/cython_pid.pyx"],
-        include_dirs=[np.get_include()]
-    ),
-]
-
 setup(
     name='pointsim',
     version='0.0.1',
@@ -38,3 +29,11 @@ setup(
     },
     python_requires='>=3.9'
 )
+# Cython-модуль, который нужно скомпилировать
+extensions = [
+    Extension(
+        "pointsim.cython_pid",
+        ["pointsim/cython_pid.pyx"],
+        include_dirs=[np.get_include()]
+    ),
+]
