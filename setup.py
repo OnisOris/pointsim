796ff1fe6f9caa37584d96b1a76215658cd55109
Michael <onisoris@yandex.ru>
2024-10-26 23:53:59 +0300

    Новый setup.py c установкой cython модуля

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
-from setuptools import setup, find_packages
-
+from setuptools import setup, find_packages, Extension
+from Cython.Build import cythonize
+import numpy as np
 
 def readme():
     with open('README.md', 'r', encoding='utf-8') as f:
         return f.read()
 
+# Cython-модуль, который нужно скомпилировать
+extensions = [
+    Extension(
+        "pointsim.cython_pid",
+        ["pointsim/cython_pid.pyx"],
+        include_dirs=[np.get_include()]
+    ),
+]
 
 setup(
     name='pointsim',
@@ -16,7 +25,8 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/OnisOris/pointsim',
     packages=find_packages(),
-    install_requires=['numpy', 'matplotlib', 'PyQt5'],
+    ext_modules=cythonize(extensions),  # Компиляция Cython
+    install_requires=['numpy', 'matplotlib', 'PyQt5', 'cython'],
     classifiers=[
         'Programming Language :: Python :: 3.12',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
